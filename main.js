ed3402935cfd70d3c11c5a5455b7d31f952a279c
andrumahardi <andrumahardi77@gmail.com>
2020-02-12 12:09:06 +0700

    added vuejs

--- main.js
+++ main.js
@@ -1,3 +1,72 @@
+const temporaryTask1 = [
+{
+    title: `ini adalah title dari task`,
+    tag: `low priority`
+},
+{
+    title: `ini adalah title dari task`,
+    tag: `high priority`
+},
+{
+    title: `ini adalah title dari task`,
+    tag: `low priority`
+},
+{
+    title: `ini adalah title dari task`,
+    tag: `high priority`
+}]
+const temporaryTask2 = [
+{
+    title: `ini adalah title dari task`,
+    tag: `low priority`
+},
+{
+    title: `ini adalah title dari task`,
+    tag: `high priority`
+},
+{
+    title: `ini adalah title dari task`,
+    tag: `low priority`
+},
+{
+    title: `ini adalah title dari task`,
+    tag: `low priority`
+},
+{
+    title: `ini adalah title dari task`,
+    tag: `low priority`
+},
+{
+    title: `ini adalah title dari task`,
+    tag: `low priority`
+},
+{
+    title: `ini adalah title dari task`,
+    tag: `low priority`
+},
+{
+    title: `ini adalah title dari task`,
+    tag: `low priority`
+},
+{
+    title: `ini adalah title dari task`,
+    tag: `low priority`
+},
+{
+    title: `ini adalah title dari task`,
+    tag: `low priority`
+},
+{
+    title: `ini adalah title dari task`,
+    tag: `high priority`
+}]
+const categories = [
+    {name: "Backlog", data: temporaryTask1}, {name: 'Product', data: temporaryTask2}, {name: 'Development', data: temporaryTask1}, {name: 'Done', data: temporaryTask1}]
+
 new Vue({
-    element: 
+    el: "#app",
+    data: {
+        currentPage: 'register',
+        categories
+    }
 })
