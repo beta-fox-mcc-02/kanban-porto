ed2a1695ec76ce86466f353179191bf3758c8ec8
mcc-15 <mcc-15@mcc-15s-Mac-mini.local>
2020-02-12 15:39:06 +0700

    login register sudah tersambung front end dan backend

--- main.js
+++ main.js
@@ -1,18 +1,61 @@
 new Vue ({
     el: '#kanban',
     data: {
+        register: {
+            first_name: "",
+            last_name: "",
+            email: "",
+            password: ""
+        },
+        valLogin: {
+            email: "",
+            password: ""
+        },
         currentPage: 'register',
-        first_name: "",
-        last_name: "",
-        email: "",
-        password: "",
     },
     methods: {
         regis() {
-            this.currentPage = 'login'
+            axios({
+                method: 'post',
+                url: 'http://localhost:3000/register',
+                data: {
+                    first_name: this.register.first_name,
+                    last_name: this.register.last_name,
+                    email: this.register.email,
+                    password: this.register.password
+                }
+            })
+                .then((response) => {
+                    this.currentPage = 'login'
+                    console.log(response)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+
         },
         login() {
-            this.currentPage = 'landing'
+            axios({
+                method: 'post',
+                url: 'http://localhost:3000/login',
+                data: {
+                    email: this.valLogin.email,
+                    password: this.valLogin.password
+                }
+            })
+                .then((data) => {
+                    this.currentPage = 'landing'
+                    console.log(data)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        },
+        fetchTask() {
+
         }
+    },
+    created() {
+        
     }
 })
