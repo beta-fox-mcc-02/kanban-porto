ca9a352d0933089cf57934df9ef657809ef14c8e
gandasipayung <gandasipayung14@gmail.com>
2020-02-12 18:13:56 +0700

    - install vue - use parcel - template belum ada

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Welcome',
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style scoped>
+</style>
