01f2096ed7b14d4aced1034fe1dcbdaad38ab682
rahmandihamzah <rahmandi.dev@gmail.com>
2020-02-12 22:05:07 +0700

    added: button google sign in

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,7 @@ new Vue({
         board: false,
         addCard: false,
         editCard: false,
+        gToken: '',
         title: '',
         description: '',
         category: '',
@@ -31,7 +32,20 @@ new Vue({
         },
         signIn: function() {
             localStorage.setItem('token', 'token')
-            this.fetchTask()
+            // this.fetchTask()
+            this.board = true
+            this.toSignIn = false
+            this.toSignUp = false
+            this.isLogedIn = true
+        },
+        onSignIn: function(googleUser) {
+            var id_token = googleUser.getAuthResponse().id_token;
+            this.gToken = id_token
+            localStorage.setItem('gToken', this.gToken)
+            localStorage.setItem('token', 'token')
+            console.log(gToken)
+
+            // this.fetchTask()
             this.board = true
             this.toSignIn = false
             this.toSignUp = false
@@ -39,21 +53,27 @@ new Vue({
         },
         signOut: function() {
             localStorage.removeItem('token')
+
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function () {
+            console.log('User signed out.');
+            });
+
             this.toSignIn = true
             this.board = false
             this.isLogedIn = false
         },
-        fetchTask: function() {
-            axios.get('http://localhost:3000/task')
-                .then(response => {
-                    response.data.forEach(task => {
-                        this.tasks.push(task)
-                    });
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-        },
+        // fetchTask: function() {
+        //     axios.get('http://localhost:3000/task')
+        //         .then(response => {
+        //             response.data.forEach(task => {
+        //                 this.tasks.push(task)
+        //             });
+        //         })
+        //         .catch(err => {
+        //             console.log(err)
+        //         })
+        // },         
         addCardFunction: function(category) {
             console.log(this.title)
             console.log(this.description)
@@ -78,7 +98,7 @@ new Vue({
     },
     created() {
         if (localStorage.getItem('token')) {
-            this.fetchTask()
+            // this.fetchTask()
             this.board = true
             this.isLogedIn = true
         } else {
