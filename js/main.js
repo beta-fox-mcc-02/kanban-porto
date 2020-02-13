25c287f3bdd5a2cef9033acb5bef679911083771
hikmanisyariful <syarifulfajar017@gmail.com>
2020-02-13 11:51:59 +0700

    VUE life cyle and Computed - grouping based on category done - add task done - update and delete done - less 3r party API google sign in - less error handler

--- js/main.js
+++ js/main.js
@@ -11,15 +11,54 @@ var app = new Vue({
         email : "",
         password : ""
       },
-      currentPage: 'login',
+      currentPage: 'registerPage',
       tasks: [],
-      user : {}
+      user : {},
+      currentTask: {},
+      newTask : {
+          title : "",
+          description : ""
+      },
+      dataTask : {},
+      categories : []
     },
     computed: {
+        backlogTask () {
+            let output = []
+            this.tasks.forEach(task => {
+                if (task.CategoryId === 1) {
+                    output.push(task)
+                }
+            })
+            return output
+            // return this.doneTask.filter()
+        },
+        productTask () {
+            let output = []
+            this.tasks.forEach(task => {
+                if (task.CategoryId === 2) {
+                    output.push(task)
+                }
+            })
+            return output 
+        },
+        developmentTask () {
+            let output = []
+            this.tasks.forEach(task => {
+                if (task.CategoryId === 3) {
+                    output.push(task)
+                }
+            })
+            return output 
+        },
         doneTask () {
             let output = []
-            //
-            return this.doneTask.filter()
+            this.tasks.forEach(task => {
+                if (task.CategoryId === 4) {
+                    output.push(task)
+                }
+            })
+            return output 
         }
     },
     methods : {
@@ -85,8 +124,85 @@ var app = new Vue({
                 this.currentPage = 'loginPage'
             }
         },
-        addNewTask () {
+        showFormAdd () {
             this.currentPage = 'addTaskPage'
+        },
+        addNewTask() {
+            axios({
+                method : "POST",
+                url : "http://localhost:3000/tasks",
+                data : {
+                    title : this.newTask.title,
+                    description : this.newTask.description
+                },
+                headers : {
+                    token : localStorage.token
+                }
+            })
+                .then(response => { 
+                    this.fetch()
+                    this.currentPage = 'mainHomePage'
+                })
+                .catch(response => {
+                    console.log(response.data)
+                })
+        },
+        showFormEdit(id) {
+            this.currentPage = 'updateTaskPage'
+            axios({
+                method : "GET",
+                url : `http://localhost:3000/tasks/` + id,
+                headers : {
+                    token : localStorage.token
+                }
+            })
+                .then(response => {
+                    console.log('MASUK THEN AXIOS FIND BY PK')
+                    this.dataTask = response.data.dataTask
+                    this.categories = response.data.dataCategories
+                })
+                .catch(response => {
+                    console.log(response.data)
+                })
+            
+        },
+        updateTask(id){
+            console.log('MASUK UPDATE MAIN.JS')
+            axios({
+                method : "PUT",
+                url : `http://localhost:3000/tasks/` + id,
+                data : {
+                    title : this.dataTask.title,
+                    description : this.dataTask.description,
+                    CategoryId: +this.dataTask.CategoryId
+                },
+                headers : {
+                    token : localStorage.token
+                }
+            })
+                .then(response => {
+                    this.currentPage = 'mainHomePage'
+                    this.fetch()
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        },
+        deleteTask(id) {
+            axios({
+                method : "DELETE",
+                url : `http://localhost:3000/tasks/` + id,
+                headers : {
+                    token : localStorage.token
+                }
+            })
+                .then(response => {
+                    this.currentPage = 'mainHomePage'
+                    this.fetch()
+                })
+                .catch(err => {
+                    console.log(err)
+                })
         }
     },
     created() {
