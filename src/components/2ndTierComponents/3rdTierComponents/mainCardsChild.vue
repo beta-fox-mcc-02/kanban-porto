d8dbd74d025f100e3ad918f6b808450bef86a53a
andrumahardi <andrumahardi77@gmail.com>
2020-02-14 06:37:23 +0700

    working on update form

--- src/components/2ndTierComponents/3rdTierComponents/mainCardsChild.vue
+++ src/components/2ndTierComponents/3rdTierComponents/mainCardsChild.vue
@@ -6,9 +6,30 @@
             <span class="content-possession">{{task.User.name}}'s Task</span>
         </div>
         <span class="content-edit">
-            <i class="fas fa-ellipsis-v" v-on:click="updateTask(task)"></i>
+            <i class="fas fa-ellipsis-v" v-if="validUser" v-on:click="showUpdateForm(task.id)"></i>
+            <i class="fas fa-ellipsis-v" v-else-if="!validUser" v-bind:style="!validUser ? 'background-color: white; color: red; border-radius: 50%; cursor: not-allowed' : ''"></i>
             <i class="far fa-trash-alt" v-if="validUser" v-on:click="deleteTask(task.id)"></i>
             <i class="far fa-trash-alt" v-else-if="!validUser" v-bind:style="!validUser ? 'background-color: white; color: red; border-radius: 50%; cursor: not-allowed' : ''"></i>
+            <form class="update-form" v-if="showForm" v-on:submit.prevent="editTask">
+                <div>
+                    <label>New title</label>
+                    <input type="text" v-bind:value="title">
+                </div>
+                <div>
+                    <label>New priority</label>
+                    <select v-model="tag">
+                        <option> high priority </option>
+                        <option> low priority </option>
+                    </select>
+                </div>
+                <div>
+                    <label>Move to</label>
+                    <select v-model="changeCategory">
+                        <option v-for="categoryName in categoryNames" v-bind:key="categoryName"> {{categoryName}} </option>
+                    </select>
+                </div>
+                <button type="submit">Submit</button>
+            </form>
         </span>
   </section>
 </template>
@@ -20,7 +41,12 @@ export default {
     name: 'MainCards-Child',
     data() {
         return {
+            title: '',
+            tag: '',
             currentPage: '',
+            changeCategory: '',
+            showForm: false,
+            categoryNames: [],
             validUser: true
         }
     },
@@ -40,6 +66,28 @@ export default {
                 this.$emit('changePage', { page: 'member' })
             })
             .catch((err) => this.validUser = false)
+        },
+        showUpdateForm(taskId) {
+            let token = localStorage.getItem('access_token')
+            axios({
+                method: 'GET',
+                url: `http://localhost:3000/task/update/${taskId}`,
+                headers: { token }
+            })
+            .then((result) => {
+                this.showForm = true
+                this.validUser = true
+                this.categoryNames = result.data.categoryNames
+                this.title = result.data.updateReadyTask.title
+                console.log(this.title, this.categoryNames)
+            })
+            .catch((err) => {
+                this.validUser = false
+                console.log(err)
+            })
+        },
+        editTask(){
+            console.log(this.title, this.tag)
         }
     }
 }
